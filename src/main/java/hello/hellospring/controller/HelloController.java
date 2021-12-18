e65cf1f3a05ce520307a93b5bfa8ebd8a0b7d9b7
Gwak seyeon <ksyk1205@naver.com>
2021-12-18 17:09:27 +0900

    프로젝트 환경설정

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,17 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String Hello(Model model){
+        model.addAttribute("data","hello!");
+        return "hello";
+    }
+
+}
