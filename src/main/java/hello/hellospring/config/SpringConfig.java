d2479f22b66fd33379f3a4358226bb2c2d029007
Gwak seyeon <ksyk1205@naver.com>
2021-12-26 19:31:18 +0900

    스프링 DB 접근 기술

--- src/main/java/hello/hellospring/config/SpringConfig.java
+++ src/main/java/hello/hellospring/config/SpringConfig.java
@@ -1,7 +1,6 @@
 package hello.hellospring.config;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +8,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    /*
+    DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+    private EntityManager em;
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }*/
+
+    public final  MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+    /*
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
-    }
+        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+
+    }*/
 }
